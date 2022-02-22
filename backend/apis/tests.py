ada0ad77346109e3e461107bd3a472cf1f0b6da8
Arpad <arpad.kiss@warwick.ac.uk>
2022-02-22 19:40:56 +0000

    Added OpenAPI tests.

--- backend/apis/tests.py
+++ backend/apis/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.conf import settings
 
-# Create your tests here.
+class UserModelTests(TestCase):
+    def setUp(self):
+        pass
+
+    def test_signup_url(self):
+        pass
+
+    def test_login_url(self):
+        pass
--- backend/openapi/tests.py
+++ backend/openapi/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse_lazy
 
-# Create your tests here.
+class OpenAPITests(APITestCase):
+    def setUp(self):
+        self.schema_url = reverse_lazy('fetch-openapi-schema')
+        self.response = self.client.get(self.schema_url)
+
+    def test_can_retrieve_openapi_specification(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_openapi_specification_has_correct_content_type(self):
+        self.assertEqual(self.response.has_header('Content-Type'), True)
+        self.assertEqual(self.response.headers['Content-Type'], 'application/vnd.oai.openapi')
+
+    def test_openapi_specification_has_some_content(self):
+        self.assertGreater(len(self.response.content), 0)
