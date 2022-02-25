16eab5cd36f77f1a96ef9239648ed9cd4d176557
Isaac <nixrevolution@gmail.com>
2022-02-25 12:12:02 +0000

    Added button to upcoming sessions but is bugged

--- frontend/src/components/UpcomingSessions.tsx
+++ frontend/src/components/UpcomingSessions.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { SemanticClassificationFormat } from "typescript";
 import { GroupSession } from "../utils/endpoints";
 import DateTextProps from "./DateText";
@@ -12,7 +13,9 @@ type UpcomingSessionProps = {
 
     return (
     //   <div className="w-full rounded-lg p-2 ">
-    <button className="bg-gray-100 rounded-2xl border-gray-200 border-2 p-2 text-center h-full">
+    // TODO there are some issues with the link, if you end up clicking the button on the home screen it results in the
+    // popup flashing open and closed repeatedly
+    <Link className="bg-gray-100 rounded-2xl border-gray-200 border-2 p-2 text-center h-full" to={"/groups?id=" + session.id}>
         <div className="flex items-center space-x-4">
           <img
             alt="Session Image"
@@ -34,6 +37,6 @@ type UpcomingSessionProps = {
             <DateTextProps date={session.date} />
           </div>
         </div>
-      </button>
+      </Link>
     );
   }
