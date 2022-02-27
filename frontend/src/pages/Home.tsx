f6b84b2307875d9d04800033a85b30d3c62f5724
TPath123 <55555992+TPath123@users.noreply.github.com>
2022-02-27 14:58:18 +0000

    Cleanup home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,9 +9,12 @@ import useSWR from "swr";
 //import RoundedImage from "../components/RoundedImage";
 import Topbar from "../components/Topbar";
 import UpcomingSessions from "../components/UpcomingSessions";
-import { useUser } from "../utils/authentication";
-import { useBusinessAreas } from "../utils/business_area";
-import { GroupSessionResponse, LIST_GROUP_SESSIONS_ENDPOINT, LIST_USER_HOSTING_SESSIONS_ENDPOINT, LIST_USER_JOINED_SESSIONS_ENDPOINT } from "../utils/endpoints";
+import {
+  GroupSessionResponse,
+  LIST_GROUP_SESSIONS_ENDPOINT,
+  LIST_USER_HOSTING_SESSIONS_ENDPOINT,
+  LIST_USER_JOINED_SESSIONS_ENDPOINT,
+} from "../utils/endpoints";
 
 type ActionProps = {
   actionText: string;
@@ -129,7 +132,7 @@ function GroupSessionsInfo() {
           (otherSession) => session.id == otherSession.id
         ) &&
         !hostingSessions?.find((otherSession) => session.id == otherSession.id)
-    ).length
+    ).length;
 
   return (
     <div className="bg-gray-50 rounded-2xl border-gray-100 border-2 p-2 space-y-2">
@@ -137,24 +140,23 @@ function GroupSessionsInfo() {
         <UserGroupIcon className="mr-2 h-5 w-5" />
         Group Sessions
       </h4>
-      <div className="flex-row gap-4 flex justify-center items-center">
-        <div className="flex flex-col w-full">
-          <p className="text-m">
-            There {numAvailableSessions == 1 ? "is" : "are"} <span className="font-bold">{numAvailableSessions}</span> group session{numAvailableSessions == 1 ? " " : "s "}
-            available which match{numAvailableSessions == 1 ? "es" : ""} your interests
-          </p>
-        </div>
-        <div className="flex flex-col">
-          <Link
-            to="/groups"
-            className="py-2 px-10 flex justify-center items-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white transition ease-in duration-200 text-center font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg shadow-md"
-          >
-            Visit Sessions
-            <span>
-              <ArrowRightIcon className="ml-2 h-5 w-5" />
-            </span>
-          </Link>
-        </div>
+      <div className="flex justify-between items-center">
+        <p className="text-m">
+          There {numAvailableSessions == 1 ? "is" : "are"}{" "}
+          <span className="font-bold">{numAvailableSessions}</span> group
+          session{numAvailableSessions == 1 ? " " : "s "}
+          available which match{numAvailableSessions == 1 ? "es" : ""} your
+          interests
+        </p>
+        <Link
+          to="/groups"
+          className="py-2 px-10 flex items-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white transition ease-in duration-200 text-center font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg shadow-md"
+        >
+          Visit Sessions
+          <span>
+            <ArrowRightIcon className="ml-2 h-5 w-5" />
+          </span>
+        </Link>
       </div>
     </div>
   );
@@ -173,42 +175,34 @@ function UpcomingSessionsColumn() {
 
   console.log(allHostSessions.toString());
 
-
   let allSessions = [...allJoinedSessions, ...allHostSessions];
-  allSessions = allSessions.sort((a,b) => Date.parse(a.date) - Date.parse(b.date)).filter((c) => Date.parse(c.date) >= Date.now());
+  allSessions = allSessions
+    .sort((a, b) => Date.parse(a.date) - Date.parse(b.date))
+    .filter((c) => Date.parse(c.date) >= Date.now());
 
   return (
     <div className="bg-gray-50 rounded-2xl border-gray-100 border-2 p-2 text-center h-full">
       <h4 className="text-l sm:text-xl font-semibold flex items-center justify-center">
         <CalendarIcon className="mr-2 h-5 w-5" />
-        <div className="mb-2">
-        Upcoming Sessions
-        </div>
+        <div className="mb-2">Upcoming Sessions</div>
       </h4>
       <div className="justify justify-center grid grid-cols-1 gap-1">
-        {allSessions.length == 0 ? <div className="align-items-">You have no upcoming sessions</div> : allSessions.map((session) => (
-          <UpcomingSessions key={session.id} session={session}/>
-        ))}
+        {allSessions.length == 0 ? (
+          <div className="align-items-">You have no upcoming sessions</div>
+        ) : (
+          allSessions.map((session) => (
+            <UpcomingSessions key={session.id} session={session} />
+          ))
+        )}
       </div>
     </div>
   );
 }
 
 export default function Home() {
-  const { user } = useUser();
-  const { areas } = useBusinessAreas();
-
-
   return (
     <>
       <Topbar />
-      {/* <DashboardUserHero
-        name={user ? `${user.first_name} ${user.last_name}` : `UNKNOWN`}
-        businessArea={
-          areas.find((area) => area.id == user?.business_area)?.name ??
-          "Unknown"
-        }
-      /> */}
       <div className="bg-white w-full">
         <div className="grid grid-cols-3 mx-5 gap-5">
           <div className="col-span-2 space-y-5">
