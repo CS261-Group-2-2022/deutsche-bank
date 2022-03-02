a5c65d0efe9456dca9021c17bfe40ce45e353014
TPath123 <55555992+TPath123@users.noreply.github.com>
2022-03-02 16:02:03 +0000

    Add loading indicator

--- frontend/src/pages/GroupSessions.tsx
+++ frontend/src/pages/GroupSessions.tsx
@@ -118,9 +118,10 @@ function HideableSessionsInfo({
 
 export default function GroupSessions() {
   // Pull in session data from backend
-  const { data: allSessions = [], isValidating } = useSWR<GroupSessionResponse>(
+  const { data: allSessionsData, isValidating } = useSWR<GroupSessionResponse>(
     LIST_All_GROUP_SESSIONS_ENDPOINT
   );
+  const allSessions = allSessionsData ?? [];
   const { data: joinedSessions } = useSWR<GroupSessionResponse>(
     LIST_USER_JOINED_SESSIONS_ENDPOINT
   );
@@ -257,12 +258,37 @@ export default function GroupSessions() {
             />
           ))}
 
-          {filteredSessions.length === 0 && (
-            <h1 className="font-bold text-xl text-gray-700 mt-10 text-center">
-              There are no sessions available
-              {isFiltering && " which match your search filter"}. Maybe create a
-              new one?
-            </h1>
+          {!allSessionsData && isValidating ? (
+            <div className="flex justify-center">
+              <svg
+                className="animate-spin -ml-1 mr-3 h-20 w-20 text-gray-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            </div>
+          ) : (
+            filteredSessions.length === 0 && (
+              <h1 className="font-bold text-xl text-gray-700 mt-10 text-center">
+                There are no sessions available
+                {isFiltering && " which match your search filter"}. Maybe create
+                a new one?
+              </h1>
+            )
           )}
         </div>
       </div>
