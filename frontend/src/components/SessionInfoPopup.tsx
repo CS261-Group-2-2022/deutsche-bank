636b25ea992089086c90fb82e837cc52d2fe567a
TPath123 <55555992+TPath123@users.noreply.github.com>
2022-03-03 19:23:06 +0000

    Add session image links

--- frontend/src/components/SessionInfoPopup.tsx
+++ frontend/src/components/SessionInfoPopup.tsx
@@ -21,6 +21,7 @@ import DateText from "./DateText";
 import Popup from "./Popup";
 import CapacityText from "./CapacityText";
 import { LoadingButton } from "./LoadingButton";
+import UserAvatar from "./UserAvatar";
 
 const RegisteredUser = ({ user }: { user: User }) => {
   return (
@@ -167,95 +168,104 @@ export default function SessionInfoPopup({
       isClosing={isClosing}
       initiateClose={initiateClose}
       closeModal={closeModal}
+      className="p-0 pb-3"
     >
-      <Dialog.Title as="h3" className="leading-6 text-gray-900 space-x-2">
-        <span className="text-3xl font-black uppercase">{session?.name}</span>
-      </Dialog.Title>
-      <div className="pt-1 flex flex-wrap gap-1 text-lg">
-        {session?.skills?.map((skill) => (
-          <SessionTopicLabel key={skill.id} name={skill.name} />
-        ))}
-      </div>
-      <div className="my-2 flex items-center">
-        <img
-          alt="Session Image"
-          src="https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg"
-          className="h-14 rounded-lg"
-        />
-        <p className="ml-3">
-          {session?.host.first_name} {session?.host.last_name}
-        </p>
-      </div>
+      <img
+        src={
+          session?.image_link ??
+          "https://thinkingportfolio.com/wp-content/uploads/2015/10/02G69046.jpg"
+        }
+        className="w-full rounded-t-2xl max-h-52 object-cover"
+      />
 
-      <div className="flex flex-col">
-        <LocationText
-          location={session?.location ?? ""}
-          link={session?.virtual_link}
-        />
-        <DateText date={session?.date ?? ""} />
-
-        <CapacityText
-          capacity={session?.capacity ?? 1}
-          num_users={session?.users.length ?? 1}
-        />
-      </div>
-
-      <div className="my-2">
-        <p className="text-sm text-gray-500">{session?.description}</p>
-      </div>
-
-      {session && session.users.length > 0 && (
-        <div className="w-full my-2">
-          <Disclosure>
-            {({ open }) => (
-              <>
-                <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-gray-900 bg-gray-100 rounded-lg hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-gray-500 focus-visible:ring-opacity-75">
-                  <span>Users Signed Up</span>
-                  <ChevronUpIcon
-                    className={`${
-                      open ? "transform rotate-180" : ""
-                    } w-5 h-5 text-gray-500`}
-                  />
-                </Disclosure.Button>
-                <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500 space-y-2">
-                  {session?.users.map((user) => (
-                    <RegisteredUser key={user.id} user={user} />
-                  ))}
-                </Disclosure.Panel>
-              </>
-            )}
-          </Disclosure>
+      <div className="px-3 pt-3">
+        <Dialog.Title as="h3" className="leading-6 text-gray-900 space-x-2">
+          <span className="text-3xl font-black uppercase">{session?.name}</span>
+        </Dialog.Title>
+        <div className="pt-1 flex flex-wrap gap-1 text-md">
+          {session?.skills?.map((skill) => (
+            <SessionTopicLabel key={skill.id} name={skill.name} />
+          ))}
         </div>
-      )}
-
-      {error && (
-        <div className="block text-sm m-1 font-medium text-red-700">
-          {error}
-        </div>
-      )}
-
-      <div className="grid grid-cols-10 gap-2">
-        {!isHosting ? (
-          <LoadingButton
-            type="button"
-            className={`inline-flex justify-center col-span-8 px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md focus:outline-none ${buttonColours[0]}`}
-            isLoading={isLoading}
-            onClick={() => (hasJoined ? leaveSession() : joinSession())}
-          >
-            {hasJoined ? "Leave Session" : "Join Session"}
-          </LoadingButton>
-        ) : (
-          <p className="col-span-8 px-4 py-2 font-medium text-center text-gray-900">
-            You are hosting this session
+        <div className="my-2 flex flex-row gap-3 items-center">
+          <div>
+            <UserAvatar user={session?.host} size={12} />
+          </div>
+          <p>
+            Host: {session?.host.first_name} {session?.host.last_name}
           </p>
+        </div>
+
+        <div className="flex flex-col">
+          <LocationText
+            location={session?.location ?? ""}
+            link={session?.virtual_link}
+          />
+          <DateText date={session?.date ?? ""} />
+
+          <CapacityText
+            capacity={session?.capacity ?? 1}
+            num_users={session?.users.length ?? 1}
+          />
+        </div>
+
+        <div className="my-2">
+          <p className="text-sm text-gray-500">{session?.description}</p>
+        </div>
+
+        {session && session.users.length > 0 && (
+          <div className="w-full my-2">
+            <Disclosure>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-gray-900 bg-gray-100 rounded-lg hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-gray-500 focus-visible:ring-opacity-75">
+                    <span>Users Signed Up</span>
+                    <ChevronUpIcon
+                      className={`${
+                        open ? "transform rotate-180" : ""
+                      } w-5 h-5 text-gray-500`}
+                    />
+                  </Disclosure.Button>
+                  <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500 space-y-2">
+                    {session?.users.map((user) => (
+                      <RegisteredUser key={user.id} user={user} />
+                    ))}
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
+          </div>
         )}
-        <button
-          type="button"
-          className={`inline-flex justify-center col-span-2 px-4 py-2 text-sm font-medium border border-transparent rounded-md focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 ${buttonColours[1]}`}
-          onClick={initiateClose}
-        >
-          Close
-        </button>
+
+        {error && (
+          <div className="block text-sm m-1 font-medium text-red-700">
+            {error}
+          </div>
+        )}
+
+        <div className="grid grid-cols-10 gap-2">
+          {!isHosting ? (
+            <LoadingButton
+              type="button"
+              className={`inline-flex justify-center col-span-8 px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md focus:outline-none ${buttonColours[0]}`}
+              isLoading={isLoading}
+              onClick={() => (hasJoined ? leaveSession() : joinSession())}
+            >
+              {hasJoined ? "Leave Session" : "Join Session"}
+            </LoadingButton>
+          ) : (
+            <p className="col-span-8 px-4 py-2 font-medium text-center text-gray-900">
+              You are hosting this session
+            </p>
+          )}
+          <button
+            type="button"
+            className={`inline-flex justify-center col-span-2 px-4 py-2 text-sm font-medium border border-transparent rounded-md focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 ${buttonColours[1]}`}
+            onClick={initiateClose}
+          >
+            Close
+          </button>
+        </div>
       </div>
     </Popup>
   );
--- frontend/src/components/UpcomingSessions.tsx
+++ frontend/src/components/UpcomingSessions.tsx
@@ -43,7 +43,10 @@ const GroupSessionCard = ({ session }: { session: GroupSession }) => {
         {/* TODO: Group Session Image */}
         <img
           alt="Session Image"
-          src="https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg"
+          src={
+            session.image_link ??
+            "https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg"
+          }
           className="h-20 rounded-lg"
         />
 
