e0cd13d4e8fe7ff9ae58772e83dd6188078dee04
Ridwanah <30591028+Ridwanah@users.noreply.github.com>
2022-03-12 19:08:00 +0000

    Update tests.py
    Added more tests for joining/unregistering for group sessions

--- backend/apis/tests.py
+++ backend/apis/tests.py
@@ -603,6 +603,131 @@ class UserModelTests(TestCase):
 
         self.assertEqual(response.status_code, 400, msg=show_res(response))
         self.assertNotEqual(response.status_code, 200)
+        
+        
+    def test_user_cannot_join_a_full_group_session(self):
+        
+        skills = list(Skill.objects.all())
+        host = User.make_random()
+        expertise_of_host = random.choice(skills) # Pick 1
+        host.expertise.set([expertise_of_host])
+        host.save()
+
+        participant = User.make_random()
+        participant.save()
+        user = User.make_random()
+        user.save()
+
+        new_groupsession = GroupSession.objects.create(name=lorem_random(max_length=100),
+                            location=lorem_random(max_length=100),
+                            virtual_link=lorem_random(max_length=100),
+                            image_link= lorem_random(max_length=100),
+                            description=lorem_random(max_length=2000),
+                            host=host,
+                            capacity=1,
+                            date =time_start + random_delta())
+        new_groupsession.skills.set([expertise_of_host])
+        new_groupsession.users.set([participant])
+        new_groupsession.save()
+        self.assertIn(participant, new_groupsession.users.all())
+        body = {
+            "session": new_groupsession.pk,
+            "user":user.pk
+        }
+        factory = APIRequestFactory()
+        request = factory.post('/api/v1/session/join',
+                               json.dumps(body),
+                               follow=True, content_type='application/json')
+        force_authenticate(request, user=user)
+
+        view = GroupSessionViewSet.as_view({'post': 'join'})
+        response = view(request, pk=new_groupsession.pk)
+        #response.render()
+
+        self.assertEqual(response.status_code, 400, msg=show_res(response))
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_user_can_join_group_session(self):
+        skills = list(Skill.objects.all())
+        host = User.make_random()
+        expertise_of_host = random.choice(skills) # Pick 1
+        host.expertise.set([expertise_of_host])
+        host.save()
+
+        participant = User.make_random()
+        participant.save()
+        new_groupsession = GroupSession.objects.create(name=lorem_random(max_length=100),
+                            location=lorem_random(max_length=100),
+                            virtual_link=lorem_random(max_length=100),
+                            image_link= lorem_random(max_length=100),
+                            description=lorem_random(max_length=2000),
+                            host=host,
+                            capacity=random.randint(1,200),
+                            date =time_start + random_delta())
+        new_groupsession.skills.set([expertise_of_host])
+        new_groupsession.save()
+
+        body = {
+            "session": new_groupsession.pk,
+            "user":participant.pk
+        }
+        factory = APIRequestFactory()
+        request = factory.post('/api/v1/session/join',
+                               json.dumps(body),
+                               follow=True, content_type='application/json')
+        force_authenticate(request, user=participant)
+
+        view = GroupSessionViewSet.as_view({'post': 'join'})
+        response = view(request, pk=new_groupsession.pk)
+        #response.render()
+
+        self.assertEqual(response.status_code, 200, msg=show_res(response))
+        
+    def test_user_can_unregister_for_group_session(self):
+        skills = list(Skill.objects.all())
+        host = User.make_random()
+        expertise_of_host = random.choice(skills) # Pick 1
+        host.expertise.set([expertise_of_host])
+        host.save()
+
+        participant = User.make_random()
+        participant.save()
+        
+
+        new_groupsession = GroupSession.objects.create(name=lorem_random(max_length=100),
+                            location=lorem_random(max_length=100),
+                            virtual_link=lorem_random(max_length=100),
+                            image_link= lorem_random(max_length=100),
+                            description=lorem_random(max_length=2000),
+                            host=host,
+                            capacity=random.randint(1, 150),
+                            date =time_start + random_delta())
+
+        new_groupsession.skills.set([expertise_of_host])
+        new_groupsession.users.set([participant])
+        new_groupsession.save()
+        self.assertIn(participant, new_groupsession.users.all())
+        a = list(participant.get_sessions())
+        self.assertTrue(a == [new_groupsession])
+        body = {
+            "session": new_groupsession.pk,
+            "user":participant.pk
+        }
+        factory = APIRequestFactory()
+        request = factory.post('/api/v1/session/leave',
+                               json.dumps(body),
+                               follow=True, content_type='application/json')
+        force_authenticate(request, user=participant)
+
+        view = GroupSessionViewSet.as_view({'post': 'leave'})
+        response = view(request, pk=new_groupsession.pk)
+        response.render()
+
+        self.assertEqual(response.status_code, 200, msg=show_res(response))
+        self.assertNotEqual(response.status_code, 400)
+
+        
+    
 
 class ActionPlanTestCase(TestCase):
     @classmethod
